1ac755d25ae6c2f7762a891e0a1f1da96d006c7a
Alex Hernandez <alexandre_2420@outlook.com>
2021-01-20 10:54:58 -0500

    Promesas

--- src/js/home2.js
+++ src/js/home2.js
@@ -5,3 +5,51 @@ function cambiarNombre(nuevoNombre){
     cambia = nuevoNombre
 }
 
+// ---promesa
+/*
+const getUserAll = new Promise(function(todoBien, todoMal){
+    //llamar API 
+    setTimeout(function(){
+        // todoMal('se acabo el tiempo p1')
+        todoBien('TODO BIEN p1')
+    }, 5000)
+})
+
+*/
+
+
+// arrow function
+const getUserAll = new Promise((todoBien, todoMal) => {
+    setTimeout(() => {
+        todoBien('Todo va bien')
+    }, 5000)
+})
+
+const getUser = new Promise(function(todoBien, todoMal){
+    //llamar API 
+    setTimeout(function(){
+        // todoMal('se acabo el tiempo')
+        todoBien('TODO BIEN p2')
+    }, 3000)
+})
+/*
+getUser
+    .then(function(){
+        console.log('todo salio bien')
+    })
+    .catch(function(message){
+        console.log(message)
+    })
+*/
+
+//para varias promesas
+Promise.all([
+    getUserAll,
+    getUser
+])
+.then(function(message){
+    console.log(message)
+})
+.catch(function(message){
+    console.log(message)
+})
